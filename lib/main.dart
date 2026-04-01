42c614e2cd9c701f2b243e640d926e92baccab4c
appropose023-tech <appropose023@gmail.com>
2026-04-01 15:28:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'capture_screen.dart';
 
 void main() {
-  runApp(const PCBApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const PCBInspectorApp());
 }
 
-class PCBApp extends StatelessWidget {
-  const PCBApp({super.key});
+class PCBInspectorApp extends StatelessWidget {
+  const PCBInspectorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "PCB Inspector",
-      debugShowCheckedModeBanner: false,
+      title: 'PCB Inspector',
+      theme: ThemeData.dark(),
       home: const CaptureScreen(),
     );
   }
